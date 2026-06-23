1be306e80e67a172026a198a2e6a933f2497c183
priyathanu <154829901+PRIYAVARSHINI-T@users.noreply.github.com>
2026-06-23 15:17:48 +0530

    Update fp4_defines.v

--- rtl/fp4_defines.v
+++ rtl/fp4_defines.v
@@ -1,1 +1,12 @@
+`ifndef FP4_DEFINES_V
+`define FP4_DEFINES_V
 
+    `define FP4_WIDTH       4
+    `define FP4_SIGN        3
+    `define FP4_EXP_MSB     2
+    `define FP4_EXP_LSB     1
+    `define FP4_MANT        0
+    `define FP4_BIAS        1
+    `define SCALE_Q4_4      16  // 2^4, representing 1.0 as 16
+
+`endif
